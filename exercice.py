94f74e3a9b5a1b5da8989b1f2a54f4eddf310465
Ohdrak <nilscoulier44@gmail.com>
2020-09-22 22:18:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def convert_to_absolute(number: float) -> float:
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-    word_l
+    word_list[]
     for pre in prefixes:
     return [""]
 
