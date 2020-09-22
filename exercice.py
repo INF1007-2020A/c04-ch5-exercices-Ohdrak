d894ab77ebed6aa2037a370923ff669d03df1b19
Ohdrak <nilscoulier44@gmail.com>
2020-09-22 22:35:47 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,6 +23,7 @@ def use_prefixes() -> List[str]:
 def is_prime_number(number):
     for i in range(2, number//2)
 def prime_integer_summation() -> int:
+    prime
     primal_number = []
     while len(primal_number) < 100:
         for i in range()
